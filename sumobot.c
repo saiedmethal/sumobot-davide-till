044e01ed6c15092aef81ea4bbe0a1340bb7fae78
till.riemer <till.riemer@gmail.com>
2012-01-18 15:44:28 +0000

    motors and lcd working, first things for IR done, next to do is implementing delay

--- sumobot.c
+++ sumobot.c
@@ -3,38 +3,56 @@
 
 */
 
-
-#include <stdio.h>
 #include <stdlib.h>
 #include <avr/io.h>
 #include "init.h"
 #include "LCD_driver.h"
 #include "LCD_functions.h"
 
+#define NO_MOVE 		750
+#define LEFT 			OCR1B
+#define RIGHT			OCR1A
+
+
 void setMotorSpeed(int, int);
+
 
 int main(void)
 {
 	LCD_Init();
 	motorInit();
-	
+	*state = StateInit();
+	secondsPassed = 0;
 
-	setMotorSpeed(1, 850);
-	setMotorSpeed(2, 650);
+	setMotorSpeed(100, 100);
+	delay(1000);
+	setMotorSpeed(-100, -100);
+	delay(2000);
+	setMotorSpeed(100, -100);
 
 	while(1){
-		LCD_puts("Hello fucker", 0);		
+		LCD_puts("running", 0);
+
+		
+		
 	}
 
 	return 0;
 }
 
-/* sets the speed of the left and right motor, no movement is 750 - remember that the right motor 
-is mirrowed to the left */
-void setMotorSpeed(int motor, int speed)
+
+/* sets the speed of the left and right motor */
+void setMotorSpeed(int left, int right)
 {
+	// set motor timers...
 	cli();
-	if(motor==1)		OCR1B = speed;
-	else if (motor==2) 	OCR1A = speed;
+	LEFT = NO_MOVE + left;
+	RIGHT = NO_MOVE - right;
 	sei();
+
+	// update state struct...
+	state->leftMotor = left;
+	state->rightMotor = right;
+	if(left==0 && right==0) state->isStopped = 1;
+	else state->isStopped = 0;
 }
