2ddadcb0f66a2bc4b077cbeab789d0013a323507
till.riemer <till.riemer@gmail.com>
2012-02-08 14:16:41 +0000

    LEDs working

--- QTISensor.c
+++ QTISensor.c
@@ -4,18 +4,15 @@ static uint32_t _black=DEFAULT_BLACK;
 
 void initialize_QTI(){
 	QTI_DDR |= (1<<LR);
-	QTI_PORT &= ~(1<<LR);
-
 	QTI_DDR |= (1<<RR);
+	QTI_PORT &= ~(1<<LR);
 	QTI_PORT &= ~(1<<RR);
 }
 
 void calibrate_QTI(){
 	_black = MAX_BLACK;
-	_black = (right_raw()+left_raw())/2;
+	_black = (right_raw()+left_raw());
 }
-
-/*** Left side ***/
 
 static inline void discharge_left(){
 	/* R HIGH output */
@@ -25,16 +22,34 @@ static inline void discharge_left(){
 	delay(5);
 }
 
+static inline void discharge_right(){
+	/* R HIGH output */
+	/* 1 ms pause */
+	QTI_DDR |= (1<<RR);
+	QTI_PORT |= (1<<RR);
+	delay(5);
+}
+
 static inline void start_charge_left(){
 	/* R LOW input */
 	QTI_PORT &= ~(1<<LR);
 	QTI_DDR &= ~(1<<LR);
 }
 
+static inline void start_charge_right(){
+	/* R LOW input */
+	QTI_PORT &= ~(1<<RR);
+	QTI_DDR &= ~(1<<RR);
+}
 
 static inline uint8_t read_left(){
 	/* R input value */
 	return bit_is_set(QTI_PIN, LR);
+}
+
+static inline uint8_t read_right(){
+	/* R input value */
+	return bit_is_set(QTI_PIN, RR);
 }
 
 uint32_t left_raw(){
@@ -51,31 +66,6 @@ uint32_t left_raw(){
 	return t;
 }
 
-uint8_t left_outside(){
-	return left_raw()<_black;
-}
-
-/*** Right side ***/
-
-static inline void discharge_right(){
-	/* R HIGH output */
-	/* 1 ms pause */
-	QTI_DDR |= (1<<RR);
-	QTI_PORT |= (1<<RR);
-	delay(5);
-}
-
-static inline void start_charge_right(){
-	/* R LOW input */
-	QTI_PORT &= ~(1<<RR);
-	QTI_DDR &= ~(1<<RR);
-}
-
-static inline uint8_t read_right(){
-	/* R input value */
-	return bit_is_set(QTI_PIN, RR);
-}
-
 uint32_t right_raw(){
 	uint32_t t=0;
 
@@ -88,6 +78,10 @@ uint32_t right_raw(){
 	sei();
 	
 	return t;
+}
+
+uint8_t left_outside(){
+	return left_raw()<_black;
 }
 
 uint8_t right_outside(){
